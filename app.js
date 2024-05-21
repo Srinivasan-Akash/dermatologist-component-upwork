7ab52ade5c71d287e76a209c6aca0fbb1fa3d38a
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-05-21 10:06:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,234 @@
+const dermal_data = {
+    "area1": {
+        "title": "Nose",
+        "description": "Individuals with a dorsal hump, nasal asymmetry, nasal depressions, or nasal deviations may seek to alter the appearance of their nose by introducing volume. Adding volume can aid in addressing these concerns by smoothing out irregularities, creating symmetry, and a balanced nasal profile."
+    },
+    "area3": {
+        "title": "Brow Tail",
+        "description": "Brow drooping refers to the descent of the brows which can lead to facial asymmetry. This can occur over time due to repetitive facial expressions, like squinting or furrowing of the brows, as the skin around this area is prone to fold and wrinkle. Aging also contributes to brow drooping."
+    },
+    "area4": {
+        "title": "Tear Troughs",
+        "description": "The tear troughs are located beneath the eyes, extending from the inner corner of the eye down towards the mid-cheek area. They are the hollowed or sunken areas that can develop under the lower eyelids, often creating a shadow or dark circle appearance. Individuals may seek to introduce volume to sunken tear troughs to reduce the sunken appearance."
+    },
+    "area5": {
+        "title": "Cheeks",
+        "description": "Aging, genetics and lifestyle choices can lead to a loss of firmness and definition in the cheeks which can contribute to a flattened or sagging appearance of the cheek area. Addressing these concerns involve restoring volume to lift and contour of the cheek area."
+    },
+    "area6": {
+        "title": "Ear Lobes",
+        "description": "Drooping of the earlobe can occur when individuals wear heavy or dangling earrings over time. Ear lobes can also be prone to losing volume and become thing and wrinkled as a result of aging. Individuals with these concerns may want to restore volume or provide extra support for their earlobe to prevent further stretching."
+    },
+    "area7": {
+        "title": "NLF",
+        "description": "The NLF or nasolabial folds extend from the sides of the nose to the corners of the mouth. These folds can vary in depth and develop in prominence as the cheek area descends.  Individuals with a NLF may want to reintroduce volume to smooth the appearance of these folds."
+    },
+    "area8": {
+        "title": "Jawline",
+        "description": "The loss of elasticity and firmness due to aging can lead to a less defined and sagging jawline, however individuals of any age can have a less defined jawline. This can be caused by factors such as genetics, weight fluctuations, and facial structure which also influence jawline appearance. Individuals with these concerns may seek to reintroduce volume and contour, aiming to create a more sculpted and defined jawline."
+    },
+    "area9": {
+        "title": "Lips",
+        "description": "Thin lips can be a sign of aging, genetics or lifestyle choices. Individuals with thin lips may want to introduce volume to their lips. Introducing volume can contribute to the appearance of a more defined lip border, create symmetry and reduce the appearance of fine lines around the facial area."
+    },
+    "area10": {
+        "title": "Accordian Lines",
+        "description": "Accordion lines are lines that develop on either side of the mouth spanning to the lower cheek when the skin becomes thinner and weaker. They are a common sign of aging created through repetitive movement through smiling which cause the lower cheek skin to bunch and crease."
+    },
+    "area11": {
+        "title": "Buccal Hollows",
+        "description": "Buccal Hollows which are located on the lower portion of the face along the sides and below the cheekbones, can develop as a result of age-related volume loss in the midface region. These hollows, also known as 'sunken cheeks,' can occur due to such as genetics, lifestyle, and the natural aging process which can exacerbate the prominence of buccal hollows. Addressing these concerns often involves restoring lost volume to the midface area"
+    },
+    "area12": {
+        "title": "Marionette",
+        "description": "Marionette lines named for their resemblance to the strings on a marionette puppet are lines starting at the corners of the mouth that extend vertically downwards to laterally circumscribe the chin. Individuals with marionette lines may wish to reintroduce volume into these lines to smooth their initial appearance."
+    },
+    "area13": {
+        "title": "Mental Crease",
+        "description": "The Metal crease is the horizontal crease that can be found underneath the lower lip and above the chin. This crease can appear as a single groove or as multiple folds and wrinkles of varying depth. Individuals with a mental crease may want to reintroduce volume to smooth the appearance of this crease. "
+    },
+    "area14": {
+        "title": "Jowls",
+        "description": "Jowls are the sagging skin or fat pockets that develop along the jawline and lower cheeks. As skin loses elasticity and facial tissues weaken over time, gravity pulls downward, causing the skin to sag and creating the appearance of jowls. Factors such as genetics, sun exposure, and lifestyle habits can exacerbate the development of jowls. Individuals bothered by jowls may seek to tighten and lift the skin in the lower face, aiming to restore a more defined jawline and reduce the visible effects of aging."
+    },
+    "area15": {
+        "title": "Chin",
+        "description": "A recessed chin, chin asymmetry or the appearance of a 'double chin' can occur due to various factors such as facial bone structure, weight fluctuations, and facial proportions. Through enhancing the shape and projection of the chin, it can make the jawline appear stronger, reduce the appearance of a double chin, and strengthen the profile of the face."
+    },
+
+    "temple": {
+        "title": "Temples",
+        "description": "Located on the sides of the head, above and to the side of the eyes, and below the hairline are the temples. This is an area where the skull's bone structure is more superficial, covered by a thinner layer of tissue compared to other parts of the face. The temples can be easily felt as hollowed areas when gently pressing on the sides of the head above the ears. Individuals with temples that are noticeably sunken may seek to reintroduce volume into these areas."
+    },
+    "area16": {
+        "title": "Dental Blocks",
+        "description": "Dental block is a variation of a local anaesthetic procedure. Dental Block is administered into the oral and perioral region to reduce any discomfort or pain that may be felt during treatments. A dental block is recommended for those who are sensitive to pain."
+    },
+    "area17": {
+        "title": "Lip Lines",
+        "description": "Lip lines, medically referred to as perioral lines are the fine lines and wrinkled that develop around the mouth are, more specifically the lips. These lines can extend vertically or horizontally and vary in depth and severity, ranging from superficial fine lines to deeper creases. Individuals with lip lines may wish to reintroduce volume into these lines to smooth their initial appearance."
+    }
+
+};
+
+var anti_aging_data = {
+    "part1": {
+        "title": "Migraine",
+        "description": "A migraine is a debilitating neurological condition marked by recurrent, severe headaches often accompanied by symptoms like nausea, sensitivity to light, and visual disturbances. Individuals suffering from migraines may opt to relax muscles in their head and neck to prevent migraines before they start."
+    },
+    "part2": {
+        "title": "Forehead",
+        "description": "Forehead wrinkles are the horizontal lines on the forehead and are a result of repeated facial expressions when raising the eyebrows. Over time these wrinkles can become more pronounced. Through limiting the movement of these muscles it can prevent the formation of these lines and reduce their depth from increasing over time."
+    },
+    "part3": {
+        "title": "Frown",
+        "description": "Frown lines, also known as glabellar lines or '11 lines,' are the vertical wrinkles that form between the eyebrows as a result of repeated facial expressions like frowning or squinting. To smooth these lines individuals may opt to freeze the movement of these muscles. This prevents the lines from increasing in depth over time."
+    },
+    "part4": {
+        "title": "Brow Lift",
+        "description": "Brow drooping refers to the descent of the brows which can lead to facial asymmetry. This can occur due to repetitive facial expressions, like squinting or furrowing of the brows, over time, as the skin around this area is prone to fold and wrinkle. Aging also contributes to brow drooping."
+    },
+    "part5": {
+        "title": "Crows Feet",
+        "description": "These lines located around the outer corners of the eyes are typically caused by repeated facial expressions such as smiling, squinting, or laughing, as well as factors like sun exposure, genetics, and aging. Individuals with crow’s feet may seek to cease the movement in these muscles to prevent depth from developing further over time."
+    },
+    "part6": {
+        "title": "Tarsal Plate",
+        "description": "The tarsal plate is a firm, fibrous structure providing support to the eyelids and maintaining their shape. Some individuals may experience skin bunching in this area when they smile, this can be reduced by relaxing these muscles."
+    },
+    "part7": {
+        "title": "Bunny Scrunch",
+        "description": "Bunny scrunch lines, also known as bunny lines, are the horizontal wrinkles that develop on the bridge. These lines are often considered a sign of facial expression and can become more prominent over time due to repetitive movements. Individuals with prominent bunny lines may seek to inhibit the muscle contractions in this area which can reduce the appearance of these lines."
+    },
+    "part8": {
+        "title": "Chin",
+        "description": "The chin muscle gets stronger as individuals age and it pulls up towards the nose. This can result in a mental crease, dimpling, and loss of the chin shape as it shortens. To treat this area individuals may opt to reduce muscle movement in this area. This can aid in reducing the depth of these wrinkles, relaxing the muscle down to prevent them from worsening over time. The vertical lines or folds that develop on the chin over time are often caused by a combination of factors including aging, repetitive facial expressions, sun exposure, and genetics."
+    },
+    "part9": {
+        "title": "Accordion lines",
+        "description": "Accordion lines can develop on either side of the mouth when the skin becomes thinner and weaker. They are a common side of aging created through making facial expressions that cause the lower cheek skin to bunch and crease."
+    },
+    "part10": {
+        "title": "Masseters",
+        "description": "Overactive or hypertrophic (enlarged) masseter muscles can contribute to the appearance of a square jawline, known as masseter hypertrophy. Through relaxing this muscle it can cause the bulky muscle to shrink over time contributing to a more oval or heart shaped facial contour. Additionally individuals suffering with bruxism (teeth grinding) and temporomandibular joint (TMJ) disorders may consider treatments to relax and weaken their masseter muscles."
+    },
+    "part11": {
+        "title": "DAO",
+        "description": "DAO, or depressor anguli oris, is a muscle located at the corners of the mouth. When overactive, this muscle can contribute to the formation of downturned mouth corners. Through relaxing this muscle the corners on the mouth can begin to lift reducing the appearance of a downturned mouth."
+    },
+    "part12": {
+        "title": "Trapezius",
+        "description": "The trapezius muscle is a large, triangular-shaped muscle located in the upper back and neck region. It plays a key role in various movements of the head, neck, and shoulders. Relaxing the trapezius can contribute to the slimming of the muscle due to inhibiting over-activity."
+    },
+    "part13": {
+        "title": "Platysmal bands",
+        "description": "Platysmal bands are vertical muscle bands located in the front of the neck, extending from the jawline to the collarbone. Aging and overactivity can contribute to the prominence of how these neck bands appear. Relaxing this muscle will smooth the prominence of the neck bands over time."
+    },
+    "part14": {
+        "title": "Lip Flip",
+        "description": `Relaxing the orbicularis oris muscle, which encircles the mouth, prevents it from contracting as strongly and pulling the upper lip inwards. This can can the upper lip to evert or “flip" outward slightly, exposing more of the pink lip tissue and creating the appearance of a fuller upper lip.`
+    },
+    "part15": {
+        "title": "Perioral Lines",
+        "description": `Often referred to as “smoker's lines" or "lipstick lines”, perioral lines develop due to aging, sun exposure, genetics and repetitive facial movements, which can be caused by smoking. To smooth the appearance of these lines individuals may opt to reduce the activity of the associated muscles. `
+    },    
+    "part16": {
+        "title": "Gummy Smile",
+        "description": "A gummy smile occurs when a significant portion of the upper gum is exposed when smiling. This phenomenon is often caused by hyperactive upper lip muscles, which pull the upper lip higher than normal when smiling. By weakening the associated muscles it can reduce the elevation of the upper lip when smiling, resulting in less gum exposure."
+    },
+    "part17": {
+        "title": "Haytox",
+        "description": "Itchy eyes, sneezing, runny nose, headaches and tiredness associated with seasonal allergies can occur due to the inflammation of the nasal mucosa (lining of the nose). This can be in response to triggers such as dust mites, grasses or pet dander. Individuals suffering with hay fever may consider using treatments to block the nerve endings of the nasal mucosa, leading to a reduction in inflammation."
+    }
+
+};
+
+// Your existing jQuery code
+jQuery(($) => {
+    $('.tab').on('click', function () {
+        const tabId = $(this).data('tab');
+        console.log(tabId);
+      
+        if (!$(this).hasClass('is-active')) {
+          $('.tab').removeClass('is-active');
+          $(this).addClass('is-active');
+      
+          var scrollTo = $('#' + tabId).offset().left;
+          console.log(scrollTo);
+          $('#dermal').animate({
+            scrollLeft: scrollTo
+          }, 500);
+        }
+      });
+
+    // DERMAL DATA [INCOORECT CLASS NAMES]
+    $("#anti-aging map>area").hover(function () {
+        let area_key = $(this).data("key");
+        $("#anti-aging ." + area_key).css({ "display": "block" });
+        $("#anti-aging ." + area_key).css({ "opacity": 1 });
+    }, function () {
+        let area_key = $(this).data("key");
+        $("#anti-aging ." + area_key).css({ "opacity": 0 });
+    });
+
+    var book_now_link = "https://esteemclinic.zenoti.com/webstoreNew/services";
+    $("#book_now_link").attr("href", book_now_link);
+
+    $("#anti-aging map>area").on("click", function (e) {
+        e.preventDefault();
+
+        let area_key = $(this).data("key");
+
+        $("#anti-aging .face-area-img").removeClass("is-active").css('opacity', 0);
+        $("#anti-aging ." + area_key).addClass("is-active").css('opacity', 1);
+
+        let title = dermal_data[area_key]["title"];
+        let desc = dermal_data[area_key]["description"];
+
+        $("#anti-aging .instructions").hide();
+        $("#anti-aging .area-data").removeClass("is-active");
+
+        document.querySelector("#anti-aging .area-details").innerText = desc;
+        document.querySelector("#anti-aging .area-title").innerText = title;
+
+        setTimeout(() => {
+            $("#anti-aging .area-data").addClass("is-active");
+        }, 100);
+    });
+
+
+    //   ANTI-AGING 
+    $("#dermal-filler map>area").hover(function () {
+        let area_key = $(this).data("key");
+        $("#dermal-filler ." + area_key).css({ "display": "block" });
+        $("#dermal-filler ." + area_key).css({ "opacity": 1 });
+    }, function () {
+        let area_key = $(this).data("key");
+        $("#dermal-filler ." + area_key).css({ "opacity": 0 });
+    });
+
+    var book_now_link = "https://esteemclinic.zenoti.com/webstoreNew/services";
+    $("#book_now_link").attr("href", book_now_link);
+
+    $("#dermal-filler map>area").on("click", function (e) {
+        e.preventDefault();
+
+        let area_key = $(this).data("key");
+
+        $("#dermal-filler .face-area-img").removeClass("is-active").css('opacity', 0);
+        $("#dermal-filler ." + area_key).addClass("is-active").css('opacity', 1);
+
+        let title = anti_aging_data[area_key]["title"];
+        let desc = anti_aging_data[area_key]["description"];
+
+        $("#dermal-filler .instructions").hide();
+        $("#dermal-filler .area-data").removeClass("is-active");
+
+        document.querySelector("#dermal-filler .area-details").innerText = desc;
+        document.querySelector("#dermal-filler .area-title").innerText = title;
+
+        setTimeout(() => {
+            $("#dermal-filler .area-data").addClass("is-active");
+        }, 100);
+    });
+});
